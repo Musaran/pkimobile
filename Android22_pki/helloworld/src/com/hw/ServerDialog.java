c1cf23980ab7ddde9f91690aa21a21b2dc87bca2
william.jouot@gmail.com <william.jouot@gmail.com@086c8089-4ca5-43aa-5091-817c183f888e>
2011-02-11 23:45:51 +0000

    Début de serveur. Encore quelques bugs avec la lecture de byte[] (NullException).

---
+++ Android22_pki/helloworld/src/com/hw/ServerDialog.java
@@ -0,0 +1,95 @@
+package com.hw;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ServerDialog
+{
+	private Socket sk;
+	//private PrintStream versServeur;
+	//private BufferedReader depuisServeur;
+	private boolean isConnected = false;
+	
+	private DataInputStream fromServer;
+	private DataOutputStream toServer;
+	
+	private void disconnect()
+	{
+		if(isConnected)
+		{
+			isConnected = false;
+			try	{
+				sk.close();
+			} catch ( Exception e ){e.printStackTrace();}
+		}
+	}
+	
+	private void connect( String ip, int port )
+	{
+		try
+		{
+			sk = new Socket( ip, port );
+			//versServeur = new PrintStream(new DataOutputStream(sk.getOutputStream()) );
+			//depuisServeur = new BufferedReader(new InputStreamReader(sk.getInputStream()));
+			fromServer = new DataInputStream(sk.getInputStream());
+			toServer = new DataOutputStream(sk.getOutputStream());
+			isConnected = true;
+		}
+		catch( Exception e ){e.printStackTrace(); isConnected = false;}
+	}
+	
+	private void send( byte[] message )
+	{
+		if(isConnected)
+		{
+			try {
+				//versServeur.write(message); //versServeur.print( message );
+				toServer.write(message);
+				toServer.flush();
+			} catch (IOException e) {e.printStackTrace();}
+		}
+	}
+	
+	private byte[] receive( int timeout )
+	{
+		if(isConnected)
+		{
+			boolean ok = true;
+			long now = System.currentTimeMillis();
+			byte[] res = null;
+			
+			while( ok )
+			{
+				timeout = timeout * 1000;
+				if( ( System.currentTimeMillis() - now ) >= timeout )
+				{
+					ok = false;
+				}
+				else
+				{
+					try
+					{
+						//line = depuisServeur.readLine();
+						fromServer.readFully(res);
+					}
+					catch( Exception e ){e.printStackTrace();}
+				}
+			}
+			
+			return res;
+		}
+		else
+			return null;
+	}
+	
+	public byte[] getFromServer(String ip, int port, byte[] msg)
+	{
+		connect(ip, port);
+		send(msg);
+		byte[] res = receive(5);
+		disconnect();
+		return res;
+	}
+}
