571fcab280db4f972458edadaed54cf733a37b92
william.jouot@gmail.com <william.jouot@gmail.com@086c8089-4ca5-43aa-5091-817c183f888e>
2011-02-23 22:55:01 +0000

    Bon, j'ai commencer à un peu documenter tout le code. J'ai rajouté un champ pour entrer l'adresse IP du serv, et j'ai commencer à implémenter l'encodage coté serveur pour la réponse en cas de succès, mais c'est pas fini. Il reste plus que ça, décoder coté client et c'est tout bon !

--- trunk/PKIServer/src/Client.java
+++ trunk/PKIServer/src/Client.java
@@ -1,11 +1,10 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.security.Key;
 import java.security.KeyFactory;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
 import java.security.spec.X509EncodedKeySpec;
@@ -20,6 +19,11 @@ class Client implements Runnable
 	private DataOutputStream toClient;
 	private Server parent;
 
+	/**
+	 * Create and manage a new client connection (the mobile)
+	 * @param client	the socket of the client
+	 * @param s			the pointer to the server
+	 */
 	public Client( Socket client, Server s )
 	{
 		this.client = client;
@@ -35,33 +39,23 @@ class Client implements Runnable
 		new Thread( this ).start();
 	}
 
+	/**
+	 * Look for receive message and answer
+	 */
 	public void run()
 	{
 		try	{
+			// We receive a message...
+			// So we decrypte it
 			byte[] read = new byte[65534];
 			int nb = fromClient.read(read);
-			System.out.println(" [*] Recu "+nb+" bytes");//: [0]"+read[0]+" [H]"+read.hashCode());
+			System.out.println(" [*] Recu "+nb+" bytes");
 			byte[] rec = new byte[nb];
-			//String l = "";
-			for(int i=0; i < nb; i++) {
-				rec[i] = read[i];
-				//l += rec[i]+"/";
-			}
-			//System.out.println("Refac: [0]"+rec[0]+" [H]"+rec.hashCode());
-			//System.out.println(l);
+			for(int i=0; i < nb; i++) {	rec[i] = read[i]; }
 			
 			byte[] decrypted = decryptText(rec, parent.getPrivateKey());
 			
-			// DEBUG
-			// ----------------------------------
-			/*System.out.println("Decrypted: ");
-			String l = "";
-			for(int i=0; i < 20; i++) {
-				l += decrypted[i]+"/";
-			}
-			System.out.println(l);*/
-			// ----------------------------------
-			
+			// If the first byte is 1 then it's a connexion attempt
 			if(decrypted[0] == 1)
 			{
 				System.out.println("[0x01] Tentative de connexion");
@@ -69,27 +63,37 @@ class Client implements Runnable
 				try {
 					if(parent.isFileExists("client.pub"))
 					{
+						// We load the public key of the client
 						KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 						X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(parent.getFile("client.pub"));
 						PublicKey clp  = keyFactory.generatePublic(publicKeySpec);
 						System.out.println("[0x01] Cle chargee");
 						
+						// We verify if the message start by "CHALLENGE"
 						byte[] message = "CHALLENGE".getBytes();
 						byte[] rcvmsg = new byte[message.length];
 						System.arraycopy(decrypted, 1, rcvmsg, 0, message.length); // BOF inside
 						if(Arrays.equals(message, rcvmsg))
 						{
+							// Yep, so we check the signature
 							System.out.println("[0x01] Challenge recu. Verification de la signature...");
 							int signlgth = (decrypted.length - (message.length + 1));
 							byte[] sign = new byte[signlgth];
 							System.arraycopy(decrypted, (1+message.length), sign, 0, signlgth);
-							boolean res = verifySignature(clp, message, sign);
+							boolean res = verifySignature( message, sign, clp );
 							if(res)
 							{
+								// Everything is OK
 								System.out.println("[0x01] Signature CORRECTE !");
 								System.out.println("[0x01] ('''''''========= WIN =========='''''')");
-								byte[] ret = {1};
-								this.send(ret);
+								
+								// We send the crypted response
+								byte[] ret = "AUTH".getBytes();
+								byte[] retsign = getSignature(ret, parent.getPrivateKey());
+								byte[] msgfinal = new byte[ret.length + retsign.length];
+								System.arraycopy(ret, 0, msgfinal, 0, ret.length);
+								System.arraycopy(retsign, 0, msgfinal, ret.length, retsign.length);
+								this.send(encryptText(msgfinal, parent.getPublicKey()));
 							}
 							else
 							{
@@ -114,7 +118,9 @@ class Client implements Runnable
 			}
 			else if(decrypted[0] == 2)
 			{
+				// First byte is 2, so we receive a public key encrypted with our private key
 				try {
+					// Extract and create the public key
 					System.out.println("[0x02] Reception de la cle publique...");
 					byte[] publickey = new byte[decrypted.length-1];
 					System.arraycopy(decrypted, 1, publickey, 0, decrypted.length-1);
@@ -123,18 +129,10 @@ class Client implements Runnable
 					PublicKey clp  = keyFactory.generatePublic(publicKeySpec);
 					System.out.println("[0x02] Cle publique recue");
 					
-					// DEBUG
-					// ----------------------------------
-					/*System.out.println("[0x02] Cle recue : ");
-					String l = "";
-					for(int i=0; i < 20; i++) {
-						l += publickey[i]+"/";
-					}
-					System.out.println(l);*/
-					// ----------------------------------
-					
+					// Put it in a file
 					parent.setFile("client.pub", clp.getEncoded());
 					
+					// Send the OK response
 					System.out.println("[0x02] Envoi reponse 1");
 					byte[] res = {1};
 					this.send(res);
@@ -155,6 +153,10 @@ class Client implements Runnable
 		stop();
 	}
 
+	/**
+	 * Send a message to the client
+	 * @param msg	the message to send
+	 */
 	public void send( byte[] msg )
 	{
 		try {
@@ -162,6 +164,9 @@ class Client implements Runnable
 		} catch (IOException e) { e.printStackTrace(); }
 	}
 
+	/**
+	 * Stop the connexion and disconnect the client
+	 */
 	public void stop()
 	{
 		try	{
@@ -170,6 +175,11 @@ class Client implements Runnable
 		} catch(IOException e){e.printStackTrace();}
 	}
 	
+	/**
+	 * Print the first 20 bytes of an array of bytes. Used for debug.
+	 * @param e	the array of byte
+	 * @return the represented string
+	 */
 	public String printBytes(byte[] e)
 	{
 		String l = "";
@@ -181,54 +191,97 @@ class Client implements Runnable
 		return l;
 	}
 	
-	/* ---------------- PKI -------------------- */
-	public byte[] decryptText( byte[] btext, Key pri )
+	/* ---------------------------------------------------------------------------------- */
+	/* ---------------------------------------------------------------------------------- */
+	/* ---------------------------------      PKI       --------------------------------- */
+	/* ---------------------------------------------------------------------------------- */
+	/* ---------------------------------------------------------------------------------- */
+	/**
+	 * Encrypt a text
+	 * @param text	the text which must be crypted
+	 * @param pub	the public key
+	 * @return		the crypted text
+	 */
+	public byte[] encryptText( byte[] text, PublicKey pub )
 	{
 		try
 		{
 			DataOutputStream eOutRSA = new DataOutputStream(new FileOutputStream("temp.m"));
 			Cipher cf = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-			cf.init(Cipher.DECRYPT_MODE, pri);
-			/*int bigcount = 0;
-			for(int i = 0; i < btext.length; i+=117)
-			{
-				byte[] te = new byte[117]; int count = 0;
-				for(int j = i; j < (i+117); j++)
-				{
-					if(j < btext.length)
-					{
-						te[count] = btext[j];
-						count++;
-						bigcount++;
-					}
-				}
-				
-				byte[] decodedMsg = cf.doFinal(te);
-				eOutRSA.write(decodedMsg, 0, decodedMsg.length);
-			}*/
-			cf.update(btext);
-			byte[] decodedMsg = cf.doFinal();
-			eOutRSA.write(decodedMsg, 0, decodedMsg.length);
-			/* --- */
+			cf.init(Cipher.ENCRYPT_MODE, pub);
+			cf.update(text);
+			byte[] encodedMsg = cf.doFinal();
+			eOutRSA.write(encodedMsg, 0, encodedMsg.length);
 			eOutRSA.close();
 			byte[] res = parent.getFile("temp.m");
-			File f = new File("temp.m");
-			f.delete();
+			parent.deleteFile("temp.m");
 			return res;
 		}
 		catch( Exception e ){e.printStackTrace();}
 		return null;
 	}
 	
-	public boolean verifySignature( PublicKey pub, byte[] msg, byte[] sign )
+	/**
+	 * Decrypt a message with a private key.
+	 * @param text	the text to decrypt
+	 * @param pri	the private key used to decrypt
+	 * @return		the text decrypted
+	 */
+	public byte[] decryptText( byte[] text, PrivateKey pri )
+	{
+		try
+		{
+			DataOutputStream eOutRSA = new DataOutputStream(new FileOutputStream("temp.m"));
+			Cipher cf = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cf.init(Cipher.DECRYPT_MODE, pri);
+			cf.update(text);
+			byte[] decodedMsg = cf.doFinal();
+			eOutRSA.write(decodedMsg, 0, decodedMsg.length);
+			eOutRSA.close();
+			byte[] res = parent.getFile("temp.m");
+			parent.deleteFile("temp.m");
+			return res;
+		}
+		catch( Exception e ){e.printStackTrace();}
+		return null;
+	}
+	
+	/**
+	 * Verify if the signature is the good one
+	 * @param text	the signed text
+	 * @param sign	the signature
+	 * @param pub	the public key used to verify the signature
+	 * @return		true if the signature is the good one
+	 */
+	public boolean verifySignature( byte[] text, byte[] sign, PublicKey pub )
 	{
 		try {
 			Signature rsa = Signature.getInstance("SHA1withRSA");
 			rsa.initVerify( pub );
-			rsa.update( msg );
+			rsa.update( text );
 			boolean res = rsa.verify( sign );
 			return res;
 		}catch( Exception e ){e.printStackTrace();}
 		return false;
+	}
+	
+	/**
+	 * Get the signature of a text
+	 * @param text	the text which must be signed
+	 * @param pri	the private key
+	 * @return		the signature
+	 */
+	public byte[] getSignature( byte[] text, PrivateKey pri )
+	{
+		try
+		{
+			Signature dsa = Signature.getInstance("SHA1withRSA");
+			dsa.initSign( pri );
+			dsa.update( text );
+			byte[] signature = dsa.sign();
+			return signature;
+		}
+		catch( Exception e ){e.printStackTrace();}
+		return null;
 	}
 }
--- trunk/PKIServer/src/Server.java
+++ trunk/PKIServer/src/Server.java
@@ -7,11 +7,14 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class Server implements Runnable
 {
 	private PrivateKey privateKey = null;
+	private PublicKey publicKey = null;
 	private ServerSocket sServeur;
 	
 	public static void main(String[] args)
@@ -20,26 +23,22 @@ public class Server implements Runnable
 		Server s = new Server(1023);
 	}
 	
+	/**
+	 * Create a new listening server
+	 * @param iPort	the port to listen
+	 */
 	public Server( int iPort )
 	{
 		try
 		{
 			System.out.println(" [*] Recuperation des cles du serveur...");
-			// On recupere notre cle privee, du serveur.
-			URL url = new URL("http://williamjouot.com/pki/private.der");
-			URLConnection connection = url.openConnection();
-			InputStream input = connection.getInputStream();
-			FileOutputStream writeFile = new FileOutputStream("temp.der");
-			byte[] buffer = new byte[1024];
-			int read;
-			while ((read = input.read(buffer)) > 0)
-				writeFile.write(buffer, 0, read);
-			writeFile.flush();
-			
-			// On la charge
+
+			// On charge notre cle privee et publique
 			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(getFile("temp.der"));
+			PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(getURL("http://williamjouot.com/pki/private.der"));
 			this.privateKey = keyFactory.generatePrivate(privateKeySpec);
+			X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(getURL("http://williamjouot.com/pki/public"));
+			this.publicKey  = keyFactory.generatePublic(publicKeySpec);
 			
 			// On lance le serveur
 			System.out.println( " [*] Demarrage du serveur [port: " + iPort + "]..." );
@@ -49,6 +48,9 @@ public class Server implements Runnable
 		catch (Exception e) {e.printStackTrace();}
 	}
 	
+	/**
+	 * The thread which listen
+	 */
 	public void run()
 	{
 		System.out.println( " [*] Demarrage de l'ecoute..." );
@@ -62,17 +64,67 @@ public class Server implements Runnable
 		catch( Exception e ) { e.printStackTrace(); }
 	}
 	
+	/**
+	 * Return the private key of the server
+	 * @return the private key of the server
+	 */
 	public PrivateKey getPrivateKey()
 	{
 		return privateKey;
 	}
 	
+	/**
+	 * Return the public key of the server
+	 * @return the public key of the server
+	 */
+	public PublicKey getPublicKey()
+	{
+		return publicKey;
+	}
+	
+	/**
+	 * Return the array of bytes from an URL
+	 * @param link	the url
+	 * @return an array of bytes
+	 */
+	public byte[] getURL(String link)
+	{
+		try {
+			URL url = new URL(link);
+			URLConnection connection = url.openConnection();
+			InputStream input = connection.getInputStream();
+			FileOutputStream writeFile = new FileOutputStream("temp.der");
+			byte[] buffer = new byte[1024];
+			int read;
+			while ((read = input.read(buffer)) > 0)
+				writeFile.write(buffer, 0, read);
+			writeFile.flush();
+			return getFile("temp.der");
+		}
+		catch(Exception e){ e.printStackTrace(); }
+		return null;
+	}
+	/* ---------------------------------------------------------------------------------- */
+	/* ---------------------------------------------------------------------------------- */
+	/* ---------------------------------    FICHIERS    --------------------------------- */
+	/* ---------------------------------------------------------------------------------- */
+	/* ---------------------------------------------------------------------------------- */
+	/**
+	 * Return true if a file exists
+	 * @param filename	the file to test
+	 * @return true if it exists
+	 */
 	public boolean isFileExists(String filename)
 	{
 		File f = new File(filename);
 		return f.exists();
 	}
 	
+	/**
+	 * Get the array of bytes from a specified file
+	 * @param filename	the file to read
+	 * @return the array of bytes in the file
+	 */
 	public byte[] getFile(String filename)
 	{
 		try
@@ -87,6 +139,11 @@ public class Server implements Runnable
 		return null;
 	}
 	
+	/**
+	 * Write an array of bytes in a file
+	 * @param filename	the file to write into
+	 * @param content	the content to write
+	 */
 	public void setFile(String filename, byte[] content)
 	{
 		try
@@ -96,5 +153,15 @@ public class Server implements Runnable
 			fis.close();
 		}
 		catch (Exception e) {e.printStackTrace();}
+	}
+
+	/**
+	 * Delete a file
+	 * @param string the file to delete
+	 */
+	public void deleteFile(String filename)
+	{
+		File f = new File(filename);
+		f.delete();
 	}
 }
